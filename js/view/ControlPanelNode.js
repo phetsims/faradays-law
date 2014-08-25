94e752ba5e8b9e51ba42c0040f2c584e696990a2
Vasily Shakhov <vasily.shakhov@mlearner.com>
2014-08-25 18:55:00 +0400

    checkbox showField added

--- js/view/ControlPanelNode.js
+++ js/view/ControlPanelNode.js
@@ -12,12 +12,18 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var ResetAllButton = require( 'SCENERY_PHET/ResetAllButton' );
-  var FlipMagnetButton = require('FARADAYS_LAW/view/buttons/FlipMagnetButton');
+  var CheckBox = require( 'SUN/CheckBox' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  var FlipMagnetButton = require( 'FARADAYS_LAW/view/buttons/FlipMagnetButton' );
+
+  // strings
+  var showFieldLinesString = require( 'string!FARADAYS_LAW/faradays-law.showFieldLines' );
 
   function ControlPanelNode( model ) {
     Node.call( this );
 
-    //reset
+    // reset
     this.addChild( new ResetAllButton( {
       listener: model.reset.bind( model ),
       right: model.width - 10,
@@ -25,15 +31,22 @@ define( function( require ) {
       scale: 0.75
     } ) );
 
-    //flip magnet button
-    this.addChild( new FlipMagnetButton({
+    // flip magnet button
+    this.addChild( new FlipMagnetButton( {
       listener: function() {
         model.magnetModel.flipped = !model.magnetModel.flipped;
       },
       bottom: model.height - 9,
       right: model.width - 110,
-      opacity:0.5
-    }) );
+      opacity: 0.5
+    } ) );
+
+    // show field lines
+    this.addChild( new CheckBox( new Text( showFieldLinesString, {font: new PhetFont( 16 )} ), model.magnetModel.showFieldLinesProperty , {
+      x: 174,
+      bottom: model.height-37
+    } ));
+
 
   }
 
