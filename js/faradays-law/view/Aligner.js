17ca3c168fe2b4223d6909c9eb39668ce285d438
Sam Reid <reids@colorado.edu>
2015-10-06 13:18:17 -0600

    Fixed lint issues, see https://github.com/phetsims/chipper/issues/235

--- js/faradays-law/view/Aligner.js
+++ js/faradays-law/view/Aligner.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Encapsulates the position of wires, voltmeter, bulb objects on the screen relative to coils position.
