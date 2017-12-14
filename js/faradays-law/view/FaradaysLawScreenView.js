f843a6466b60207ef0bbb6b66946d4a930b1ba35
samreid <reids@colorado.edu>
2017-12-13 21:31:34 -0700

    Reverted trial changes, see https://github.com/phetsims/phet-io/issues/1227

--- js/faradays-law/view/FaradaysLawScreenView.js
+++ js/faradays-law/view/FaradaysLawScreenView.js
@@ -17,11 +17,8 @@ define( function( require ) {
   var ControlPanelNode = require( 'FARADAYS_LAW/faradays-law/view/ControlPanelNode' );
   var faradaysLaw = require( 'FARADAYS_LAW/faradaysLaw' );
   var FaradaysLawConstants = require( 'FARADAYS_LAW/faradays-law/FaradaysLawConstants' );
-  var HSlider = require( 'SUN/HSlider' );
   var inherit = require( 'PHET_CORE/inherit' );
   var MagnetNodeWithField = require( 'FARADAYS_LAW/faradays-law/view/MagnetNodeWithField' );
-  var Property = require( 'AXON/Property' );
-  var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var Vector2 = require( 'DOT/Vector2' );
   var VoltmeterNode = require( 'FARADAYS_LAW/faradays-law/view/VoltmeterNode' );
