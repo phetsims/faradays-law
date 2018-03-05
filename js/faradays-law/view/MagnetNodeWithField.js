0bc028634c633e2c473bf0ff9926389936e347c4
Michael Barlow <m.barlow12@gmail.com>
2018-03-05 13:36:43 -0700

    use options instead of setters for a11y properties, see
    https://github.com/phetsims/sun/issues/325

--- js/faradays-law/view/MagnetNodeWithField.js
+++ js/faradays-law/view/MagnetNodeWithField.js
@@ -44,22 +44,28 @@ define( function( require ) {
     // field lines
     this.addChild( new MagnetFieldLines( model.magnet ) );
 
+    // a11y
+    // create the focus highlight to pass as an option
+    var draggableNodeFocusHighlight = new FocusHighlightPath( new Shape() ); // overridden once the draggableNode is fully constructed
+
     // the draggable container for the magnet and arrows
-    var draggableNode = new Node( { cursor: 'pointer', tandem: tandem.createTandem( 'draggableNode' ) } );
+    var draggableNode = new Node( {
+      cursor: 'pointer',
+      tandem: tandem.createTandem( 'draggableNode' ),
+
+      // a11y
+      tagName: 'div',
+      ariaRole: 'application',
+      focusable: true,
+      focusHighlightLayerable: true,
+      focusHighlight: draggableNodeFocusHighlight
+    } );
     this.addChild( draggableNode );
+    this.addChild( draggableNodeFocusHighlight );
 
     // magnet
     self.magnetNode = createMagnetNode( model.magnet );
     draggableNode.addChild( self.magnetNode );
-
-    // a11y
-    draggableNode.tagName = 'div';
-    draggableNode.ariaRole = 'application';
-    draggableNode.focusable = true;
-    draggableNode.focusHighlightLayerable = true;
-    var draggableNodeFocusHighlight = new FocusHighlightPath( new Shape() ); // overridden once the draggableNode is fully constructed
-    draggableNode.focusHighlight = draggableNodeFocusHighlight;
-    this.addChild( draggableNodeFocusHighlight );
 
     var createArrowNode = function( tailX, tailY, tipX, tipY ) {
       var arrowNode = new ArrowNode( tailX, tailY, tipX, tipY, MAGNET_ARROW_OPTIONS );
