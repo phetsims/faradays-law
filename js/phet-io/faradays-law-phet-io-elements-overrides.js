ba335b9247b2594587b192fd212d7bff76c7f539
samreid <reids@colorado.edu>
2019-03-26 12:37:35 -0600

    Sort faradays-law overrides file, see https://github.com/phetsims/phet-io/issues/1446

--- js/phet-io/faradays-law-phet-io-elements-overrides.js
+++ js/phet-io/faradays-law-phet-io-elements-overrides.js
@@ -1,23 +1,23 @@
 /* eslint-disable */
 window.phet.phetio.phetioElementsOverrides =
   {
-    "faradaysLaw.faradaysLawScreen.model.topCoilVisibleProperty": {
+    "faradaysLaw.faradaysLawScreen.model.magnet.positionProperty": {
       "phetioFeatured": true,
-      "phetioReadOnly": true,
-      "phetioHighFrequency": true
+      "phetioReadOnly": true
     },
     "faradaysLaw.faradaysLawScreen.model.magnetArrowsVisibleProperty": {
       "phetioFeatured": true,
-      "phetioReadOnly": true,
-      "phetioHighFrequency": true
+      "phetioHighFrequency": true,
+      "phetioReadOnly": true
+    },
+    "faradaysLaw.faradaysLawScreen.model.topCoilVisibleProperty": {
+      "phetioFeatured": true,
+      "phetioHighFrequency": true,
+      "phetioReadOnly": true
     },
     "faradaysLaw.faradaysLawScreen.model.voltmeterVisibleProperty": {
       "phetioFeatured": true,
-      "phetioReadOnly": true,
-      "phetioHighFrequency": true
-    },
-    "faradaysLaw.faradaysLawScreen.model.magnet.positionProperty": {
-      "phetioFeatured": true,
+      "phetioHighFrequency": true,
       "phetioReadOnly": true
     },
     "faradaysLaw.faradaysLawScreen.view.magnetNode": {
