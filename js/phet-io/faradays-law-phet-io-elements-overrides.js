2debd20305af82b122db7f50f82fa77665d7eff5
Amanda <amanda.mcgarry@colorado.edu>
2019-04-02 13:35:13 -0600

    made activeProperty featured

--- js/phet-io/faradays-law-phet-io-elements-overrides.js
+++ js/phet-io/faradays-law-phet-io-elements-overrides.js
@@ -1,26 +1,29 @@
 /* eslint-disable */
 window.phet.phetio.phetioElementsOverrides =
-  {
-    "faradaysLaw.faradaysLawScreen.model.magnet.positionProperty": {
-      "phetioFeatured": true,
-      "phetioReadOnly": true
-    },
-    "faradaysLaw.faradaysLawScreen.model.magnetArrowsVisibleProperty": {
-      "phetioFeatured": true,
-      "phetioHighFrequency": true,
-      "phetioReadOnly": true
-    },
-    "faradaysLaw.faradaysLawScreen.model.topCoilVisibleProperty": {
-      "phetioFeatured": true,
-      "phetioHighFrequency": true,
-      "phetioReadOnly": true
-    },
-    "faradaysLaw.faradaysLawScreen.model.voltmeterVisibleProperty": {
-      "phetioFeatured": true,
-      "phetioHighFrequency": true,
-      "phetioReadOnly": true
-    },
-    "faradaysLaw.faradaysLawScreen.view.magnetNode": {
-      "phetioFeatured": true
-    }
-  };
+{
+  "faradaysLaw.faradaysLawScreen.activeProperty": {
+    "phetioFeatured": true
+  },
+  "faradaysLaw.faradaysLawScreen.model.magnet.positionProperty": {
+    "phetioFeatured": true,
+    "phetioReadOnly": true
+  },
+  "faradaysLaw.faradaysLawScreen.model.magnetArrowsVisibleProperty": {
+    "phetioFeatured": true,
+    "phetioHighFrequency": true,
+    "phetioReadOnly": true
+  },
+  "faradaysLaw.faradaysLawScreen.model.topCoilVisibleProperty": {
+    "phetioFeatured": true,
+    "phetioHighFrequency": true,
+    "phetioReadOnly": true
+  },
+  "faradaysLaw.faradaysLawScreen.model.voltmeterVisibleProperty": {
+    "phetioFeatured": true,
+    "phetioHighFrequency": true,
+    "phetioReadOnly": true
+  },
+  "faradaysLaw.faradaysLawScreen.view.magnetNode": {
+    "phetioFeatured": true
+  }
+};
