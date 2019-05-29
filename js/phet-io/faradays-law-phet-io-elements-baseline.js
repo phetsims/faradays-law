cc6c9250d1e90b98e97cd1d5f4305fc78918b8bb
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:58 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/faradays-law-phet-io-elements-baseline.js
+++ js/phet-io/faradays-law-phet-io-elements-baseline.js
@@ -1401,7 +1401,7 @@ window.phet.phetio.phetioElementsBaseline =
     "faradaysLaw.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
