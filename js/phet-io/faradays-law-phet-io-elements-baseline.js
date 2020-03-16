777e230457b270fe13bc51cac1d91fb9c719348f
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:42:00 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/faradays-law-phet-io-elements-baseline.js
+++ js/phet-io/faradays-law-phet-io-elements-baseline.js
@@ -740,7 +740,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "faradaysLaw.faradaysLawScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
