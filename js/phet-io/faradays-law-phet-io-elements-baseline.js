5cfd4cd8b8141633f0ad678b1e409be3dca79a79
zepumph <michael.kauzmann@colorado.edu>
2020-04-09 12:15:56 -0800

    rename mouseMovedAction -> mouseMoveAction, https://github.com/phetsims/scenery/issues/1043

--- js/phet-io/faradays-law-phet-io-elements-baseline.js
+++ js/phet-io/faradays-law-phet-io-elements-baseline.js
@@ -1080,7 +1080,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<Vector2IO, EventIO>"
     },
-    "faradaysLaw.general.controller.input.mouseMovedAction": {
+    "faradaysLaw.general.controller.input.mouseMoveAction": {
       "phetioDocumentation": "Emits when the mouse is moved The arguments are:<br/><ol><li>point: Vector2IO</li><br/><li>event: EventIO</li></ol>",
       "phetioDynamicElement": false,
       "phetioEventType": "USER",
