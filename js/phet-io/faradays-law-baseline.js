83f78bc772c5f733f4b82b951fb63bf97f27771c
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-10 16:53:06 -0400

    Update baseline files, see https://github.com/phetsims/joist/issues/623

--- js/phet-io/faradays-law-baseline.js
+++ js/phet-io/faradays-law-baseline.js
@@ -1328,7 +1328,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "ActionIO<NumberIO, NumberIO>"
     },
     "faradaysLaw.general.model.screenProperty": {
-      "phetioDocumentation": "Which sim screen is selected, including the home screen",
+      "phetioDocumentation": "Determines which screen is selected in the simulation",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
