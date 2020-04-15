5682263857425dd69bc1229dc166905f483e818c
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:16:29 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/faradays-law-baseline.js
+++ js/phet-io/faradays-law-baseline.js
@@ -118,19 +118,6 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "faradaysLaw.faradaysLawScreen.nameProperty": {
-      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
-      "phetioDynamicElement": false,
-      "phetioEventType": "MODEL",
-      "phetioFeatured": true,
-      "phetioHighFrequency": false,
-      "phetioIsArchetype": false,
-      "phetioPlayback": false,
-      "phetioReadOnly": false,
-      "phetioState": true,
-      "phetioStudioControl": true,
-      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
-    },
     "faradaysLaw.faradaysLawScreen.view.coilRadioButtonGroup": {
       "phetioDocumentation": "Radio button group that selects between one or two coils.",
       "phetioDynamicElement": false,
