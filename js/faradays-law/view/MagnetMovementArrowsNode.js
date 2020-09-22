d887461ecf03ac3c438ccf559be2ab8757446993
phet-dev <phetadmin@gmail.com>
2020-09-22 03:55:14 -0600

    update copyright dates from daily grunt work

--- js/faradays-law/view/MagnetMovementArrowsNode.js
+++ js/faradays-law/view/MagnetMovementArrowsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * MagnetMovementArrowsNode is a Scenery node that depicts four arrows that are used to provide a hint to the user about
