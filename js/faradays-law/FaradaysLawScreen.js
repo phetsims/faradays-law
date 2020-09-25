059aafeef808976b7043a8d9139bb4cf72e3bfc3
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:31:46 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/faradays-law/FaradaysLawScreen.js
+++ js/faradays-law/FaradaysLawScreen.js
@@ -9,34 +9,33 @@
 
 import Property from '../../../axon/js/Property.js';
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import faradaysLaw from '../faradaysLaw.js';
 import FaradaysLawConstants from './FaradaysLawConstants.js';
 import FaradaysLawModel from './model/FaradaysLawModel.js';
 import FaradaysLawScreenView from './view/FaradaysLawScreenView.js';
 
-/**
- * @param {Tandem} tandem
- * @constructor
- */
-function FaradaysLawScreen( tandem ) {
-  Screen.call( this,
-    function() {
-      return new FaradaysLawModel( FaradaysLawConstants.LAYOUT_BOUNDS, tandem.createTandem( 'model' ) );
-    },
-    function( model ) {
-      return new FaradaysLawScreenView( model, tandem.createTandem( 'view' ) );
-    }, {
-      backgroundColorProperty: new Property( 'rgb( 151, 208, 255 )' ),
-      tandem: tandem,
+class FaradaysLawScreen extends Screen {
 
-      // Cap large dt values, which can occur when the tab containing the sim had been hidden and then re-shown
-      maxDT: 0.1
-    }
-  );
+  /**
+   * @param {Tandem} tandem
+   */
+  constructor( tandem ) {
+    super(
+      function() {
+        return new FaradaysLawModel( FaradaysLawConstants.LAYOUT_BOUNDS, tandem.createTandem( 'model' ) );
+      },
+      function( model ) {
+        return new FaradaysLawScreenView( model, tandem.createTandem( 'view' ) );
+      }, {
+        backgroundColorProperty: new Property( 'rgb( 151, 208, 255 )' ),
+        tandem: tandem,
+
+        // Cap large dt values, which can occur when the tab containing the sim had been hidden and then re-shown
+        maxDT: 0.1
+      }
+    );
+  }
 }
 
 faradaysLaw.register( 'FaradaysLawScreen', FaradaysLawScreen );
-
-inherit( Screen, FaradaysLawScreen );
 export default FaradaysLawScreen;
