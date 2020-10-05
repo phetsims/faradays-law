f7d601cefdbb48a0bd42a19dc4a1533ac0905d2d
jbphet <john.blanco@colorado.edu>
2020-10-05 17:11:24 -0600

    adjusted layout to look better when voltmeter is off my default, see https://github.com/phetsims/faradays-law/issues/180

--- js/faradays-law/FaradaysLawConstants.js
+++ js/faradays-law/FaradaysLawConstants.js
@@ -11,16 +11,16 @@ import Vector2 from '../../../dot/js/Vector2.js';
 import faradaysLaw from '../faradaysLaw.js';
 
 // constants
-const BULB_POSITION = new Vector2( 190, 244 );
+const BULB_POSITION = new Vector2( 190, 200 );
 
 const FaradaysLawConstants = {
   LAYOUT_BOUNDS: new Bounds2( 0, 0, 834, 504 ),
   BULB_POSITION: BULB_POSITION,
-  VOLTMETER_POSITION: BULB_POSITION.minusXY( 0, 165 ),
+  VOLTMETER_POSITION: BULB_POSITION.minusXY( 0, 120 ),
   MAGNET_HEIGHT: 30,
   MAGNET_WIDTH: 140,
-  TOP_COIL_POSITION: new Vector2( 422, 131 ),
-  BOTTOM_COIL_POSITION: new Vector2( 448, 328 )
+  TOP_COIL_POSITION: new Vector2( 422, 110 ),
+  BOTTOM_COIL_POSITION: new Vector2( 448, 310 )
 };
 
 faradaysLaw.register( 'FaradaysLawConstants', FaradaysLawConstants );
