dbb3ce34dd59f663cea59a3434afd21afc4d25fc
pixelzoom <cmalley@pixelzoom.com>
2020-10-12 15:31:00 -0600

    convert ScreenView subclasses to ES6 subclass, https://github.com/phetsims/tasks/issues/1044

--- js/faradays-law/view/FaradaysLawScreenView.js
+++ js/faradays-law/view/FaradaysLawScreenView.js
@@ -9,7 +9,6 @@
 
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ScreenView from '../../../../joist/js/ScreenView.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import boundaryReachedSoundPlayer from '../../../../tambo/js/shared-sound-players/boundaryReachedSoundPlayer.js';
 import SoundClip from '../../../../tambo/js/sound-generators/SoundClip.js';
@@ -34,121 +33,120 @@ const summaryDescriptionString = faradaysLawStrings.a11y.summaryDescription;
 const moveMagnetToPlayString = faradaysLawStrings.a11y.moveMagnetToPlay;
 const COIL_BUMP_SOUND_LEVEL = 0.25;
 
-/**
- * @param {FaradaysLawModel} model - Faraday's Law simulation model object
- * @param {Tandem} tandem
- * @constructor
- */
-function FaradaysLawScreenView( model, tandem ) {
+class FaradaysLawScreenView extends ScreenView {
 
-  // PDOM - screen Summary
-  const summaryNode = new Node();
-  summaryNode.addChild( new Node( { tagName: 'p', innerContent: summaryDescriptionString } ) );
-  summaryNode.addChild( new Node( { tagName: 'p', innerContent: moveMagnetToPlayString } ) );
+  /**
+   * @param {FaradaysLawModel} model
+   * @param {Tandem} tandem
+   */
+  constructor( model, tandem ) {
 
-  ScreenView.call( this, {
-    layoutBounds: FaradaysLawConstants.LAYOUT_BOUNDS,
-    screenSummaryContent: summaryNode
-  } );
+    // PDOM - screen Summary
+    const summaryNode = new Node();
+    summaryNode.addChild( new Node( { tagName: 'p', innerContent: summaryDescriptionString } ) );
+    summaryNode.addChild( new Node( { tagName: 'p', innerContent: moveMagnetToPlayString } ) );
 
-  this.pdomPlayAreaNode.addChild( new CircuitDescriptionNode( model ) );
+    super( {
+      layoutBounds: FaradaysLawConstants.LAYOUT_BOUNDS,
+      screenSummaryContent: summaryNode
+    } );
 
-  // coils
-  const bottomCoilNode = new CoilNode( CoilTypeEnum.FOUR_COIL, {
-    x: model.bottomCoil.position.x,
-    y: model.bottomCoil.position.y
-  } );
+    this.pdomPlayAreaNode.addChild( new CircuitDescriptionNode( model ) );
 
-  const topCoilNode = new CoilNode( CoilTypeEnum.TWO_COIL, {
-    x: model.topCoil.position.x,
-    y: model.topCoil.position.y
-  } );
+    // coils
+    const bottomCoilNode = new CoilNode( CoilTypeEnum.FOUR_COIL, {
+      x: model.bottomCoil.position.x,
+      y: model.bottomCoil.position.y
+    } );
 
-  // @public {Vector2[]}
-  this.bottomCoilEndPositions = {
-    topEnd: bottomCoilNode.endRelativePositions.topEnd.plus( model.bottomCoil.position ),
-    bottomEnd: bottomCoilNode.endRelativePositions.bottomEnd.plus( model.bottomCoil.position )
-  };
+    const topCoilNode = new CoilNode( CoilTypeEnum.TWO_COIL, {
+      x: model.topCoil.position.x,
+      y: model.topCoil.position.y
+    } );
 
-  // @public {Vector2[]}
-  this.topCoilEndPositions = {
-    topEnd: topCoilNode.endRelativePositions.topEnd.plus( model.topCoil.position ),
-    bottomEnd: topCoilNode.endRelativePositions.bottomEnd.plus( model.topCoil.position )
-  };
+    // @public {Vector2[]}
+    this.bottomCoilEndPositions = {
+      topEnd: bottomCoilNode.endRelativePositions.topEnd.plus( model.bottomCoil.position ),
+      bottomEnd: bottomCoilNode.endRelativePositions.bottomEnd.plus( model.bottomCoil.position )
+    };
 
-  // voltmeter and bulb created
-  const voltmeterAndWiresNode = new VoltmeterAndWiresNode( model.voltmeter.needleAngleProperty, tandem.createTandem( 'voltmeterNode' ) );
-  const bulbNode = new BulbNode( model.voltageProperty, {
-    center: FaradaysLawConstants.BULB_POSITION
-  } );
+    // @public {Vector2[]}
+    this.topCoilEndPositions = {
+      topEnd: topCoilNode.endRelativePositions.topEnd.plus( model.topCoil.position ),
+      bottomEnd: topCoilNode.endRelativePositions.bottomEnd.plus( model.topCoil.position )
+    };
 
-  // wires
-  this.addChild( new CoilsWiresNode( this, model.topCoilVisibleProperty ) );
+    // voltmeter and bulb created
+    const voltmeterAndWiresNode = new VoltmeterAndWiresNode( model.voltmeter.needleAngleProperty, tandem.createTandem( 'voltmeterNode' ) );
+    const bulbNode = new BulbNode( model.voltageProperty, {
+      center: FaradaysLawConstants.BULB_POSITION
+    } );
 
-  // exists for the lifetime of the sim, no need to dispose
-  model.voltmeterVisibleProperty.link( function( showVoltmeter ) {
-    voltmeterAndWiresNode.visible = showVoltmeter;
-  } );
+    // wires
+    this.addChild( new CoilsWiresNode( this, model.topCoilVisibleProperty ) );
 
-  // When PhET-iO Studio makes the voltmeter invisible, we should also uncheck the checkbox.
-  voltmeterAndWiresNode.visibleProperty.lazyLink( function() {
-    model.voltmeterVisibleProperty.value = voltmeterAndWiresNode.visible;
-  } );
+    // exists for the lifetime of the sim, no need to dispose
+    model.voltmeterVisibleProperty.link( function( showVoltmeter ) {
+      voltmeterAndWiresNode.visible = showVoltmeter;
+    } );
 
-  // bulb added
-  this.addChild( bulbNode );
+    // When PhET-iO Studio makes the voltmeter invisible, we should also uncheck the checkbox.
+    voltmeterAndWiresNode.visibleProperty.lazyLink( function() {
+      model.voltmeterVisibleProperty.value = voltmeterAndWiresNode.visible;
+    } );
 
-  // coils added
-  this.addChild( bottomCoilNode );
-  this.addChild( topCoilNode );
-  model.topCoilVisibleProperty.linkAttribute( topCoilNode, 'visible' );
+    // bulb added
+    this.addChild( bulbNode );
 
-  // control panel
-  const controlPanel = new ControlPanelNode( model, tandem );
-  this.addChild( controlPanel );
+    // coils added
+    this.addChild( bottomCoilNode );
+    this.addChild( topCoilNode );
+    model.topCoilVisibleProperty.linkAttribute( topCoilNode, 'visible' );
 
-  // voltmeter added
-  this.addChild( voltmeterAndWiresNode );
+    // control panel
+    const controlPanel = new ControlPanelNode( model, tandem );
+    this.addChild( controlPanel );
 
-  // @private
-  this.magnetNodeWithField = new MagnetNodeWithField( model, tandem.createTandem( 'magnetNode' ) );
-  this.addChild( this.magnetNodeWithField );
-  this.pdomPlayAreaNode.accessibleOrder = [ null, this.magnetNodeWithField ];
-  this.pdomControlAreaNode.accessibleOrder = [ controlPanel ];
+    // voltmeter added
+    this.addChild( voltmeterAndWiresNode );
 
-  // move coils to front
-  bottomCoilNode.frontImage.detach();
-  this.addChild( bottomCoilNode.frontImage );
-  bottomCoilNode.frontImage.center = model.bottomCoil.position.plus( new Vector2( CoilNode.xOffset, 0 ) );
+    // @private
+    this.magnetNodeWithField = new MagnetNodeWithField( model, tandem.createTandem( 'magnetNode' ) );
+    this.addChild( this.magnetNodeWithField );
+    this.pdomPlayAreaNode.accessibleOrder = [ null, this.magnetNodeWithField ];
+    this.pdomControlAreaNode.accessibleOrder = [ controlPanel ];
 
-  topCoilNode.frontImage.detach();
-  this.addChild( topCoilNode.frontImage );
-  topCoilNode.frontImage.center = model.topCoil.position.plus( new Vector2( CoilNode.xOffset + CoilNode.twoOffset, 0 ) );
-  model.topCoilVisibleProperty.linkAttribute( topCoilNode.frontImage, 'visible' );
+    // move coils to front
+    bottomCoilNode.frontImage.detach();
+    this.addChild( bottomCoilNode.frontImage );
+    bottomCoilNode.frontImage.center = model.bottomCoil.position.plus( new Vector2( CoilNode.xOffset, 0 ) );
 
-  // ------------------------------------------------------------------------------------------------------------------
-  // sound generation
-  // ------------------------------------------------------------------------------------------------------------------
+    topCoilNode.frontImage.detach();
+    this.addChild( topCoilNode.frontImage );
+    topCoilNode.frontImage.center = model.topCoil.position.plus( new Vector2( CoilNode.xOffset + CoilNode.twoOffset, 0 ) );
+    model.topCoilVisibleProperty.linkAttribute( topCoilNode.frontImage, 'visible' );
 
-  // sounds for when the magnet bumps into the coils
-  // TODO - @Ashton-Morris - please adjust level if needed, see https://github.com/phetsims/faradays-law/issues/182
-  const lowerCoilBumpSoundClip = new SoundClip( lowerCoilBumpSound, { initialOutputLevel: COIL_BUMP_SOUND_LEVEL } );
-  soundManager.addSoundGenerator( lowerCoilBumpSoundClip );
-  const upperCoilBumpSoundClip = new SoundClip( upperCoilBumpSound, { initialOutputLevel: COIL_BUMP_SOUND_LEVEL } );
-  soundManager.addSoundGenerator( upperCoilBumpSoundClip );
-  model.coilBumpEmitter.addListener( coilType => {
-    coilType === CoilTypeEnum.FOUR_COIL ? lowerCoilBumpSoundClip.play() : upperCoilBumpSoundClip.play();
-  } );
-  model.edgeBumpEmitter.addListener( () => {
-    boundaryReachedSoundPlayer.play();
-  } );
+    // ------------------------------------------------------------------------------------------------------------------
+    // sound generation
+    // ------------------------------------------------------------------------------------------------------------------
 
-  // sound generation for voltage
-  soundManager.addSoundGenerator( new VoltageSoundGenerator( model.voltageProperty, model.voltmeterVisibleProperty ) );
+    // sounds for when the magnet bumps into the coils
+    // TODO - @Ashton-Morris - please adjust level if needed, see https://github.com/phetsims/faradays-law/issues/182
+    const lowerCoilBumpSoundClip = new SoundClip( lowerCoilBumpSound, { initialOutputLevel: COIL_BUMP_SOUND_LEVEL } );
+    soundManager.addSoundGenerator( lowerCoilBumpSoundClip );
+    const upperCoilBumpSoundClip = new SoundClip( upperCoilBumpSound, { initialOutputLevel: COIL_BUMP_SOUND_LEVEL } );
+    soundManager.addSoundGenerator( upperCoilBumpSoundClip );
+    model.coilBumpEmitter.addListener( coilType => {
+      coilType === CoilTypeEnum.FOUR_COIL ? lowerCoilBumpSoundClip.play() : upperCoilBumpSoundClip.play();
+    } );
+    model.edgeBumpEmitter.addListener( () => {
+      boundaryReachedSoundPlayer.play();
+    } );
+
+    // sound generation for voltage
+    soundManager.addSoundGenerator( new VoltageSoundGenerator( model.voltageProperty, model.voltmeterVisibleProperty ) );
+  }
 }
 
 faradaysLaw.register( 'FaradaysLawScreenView', FaradaysLawScreenView );
-
-inherit( ScreenView, FaradaysLawScreenView );
-
 export default FaradaysLawScreenView;
