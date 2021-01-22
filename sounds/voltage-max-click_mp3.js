99447e81aba20d24d96a97833dd47724eb3da6e3
zepumph <michael.kauzmann@colorado.edu>
2021-01-22 13:10:12 -0900

    use asyncLoader instead of simLauncher, plus grunt update on accident, https://github.com/phetsims/joist/issues/687

--- sounds/voltage-max-click_mp3.js
+++ sounds/voltage-max-click_mp3.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
+import asyncLoader from '../../phet-core/js/asyncLoader.js';
 import base64SoundToByteArray from '../../tambo/js/base64SoundToByteArray.js';
 import WrappedAudioBuffer from '../../tambo/js/WrappedAudioBuffer.js';
 import phetAudioContext from '../../tambo/js/phetAudioContext.js';
 
 const soundURI = 'data:audio/mpeg;base64,//swxAAABNQBdfQRABFkGq7/HtAAmHZkMyIUQG3ABoPqBAEy8EBoIS58u8o7rP/D//lHFDiw/B//8uD/aE0DKDKBEAiAAlEggEAADK81n9XhiqVr/JUX17r8JCI2ZG2OwdgmcyL3JQqHKPJJUx8YYdpOMk9f8oDtSKZxJdJKr81JFEyHss0SrR/9lF82KJeUFf+DLFJKRoAAABJK//syxAODxiBTE7wzABAAADSAAAAE4cbOle8jdCgNeZztVZ2qtkiE6RIkZgqPDXER7/xFywcLHol/98sHP6wWTEFNRTMuOTkuNaqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqv/7MsQvA8AAAaQAAAAgAAA0gAAABKqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqo=';
 const soundByteArray = base64SoundToByteArray( phetAudioContext, soundURI );
-const unlock = simLauncher.createLock( soundURI );
+const unlock = asyncLoader.createLock( soundURI );
 const wrappedAudioBuffer = new WrappedAudioBuffer();
 const onDecodeSuccess = decodedAudio => {
   wrappedAudioBuffer.audioBufferProperty.set( decodedAudio );
