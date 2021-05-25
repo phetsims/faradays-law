64700f31b7d5875377dec4c6e3edcf6541894579
phet-dev <phetadmin@gmail.com>
2021-05-25 15:45:43 -0600

    update copyright dates from daily grunt work

--- js/faradays-law/view/FaradaysLawKeyboardDragListener.js
+++ js/faradays-law/view/FaradaysLawKeyboardDragListener.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * Type to handle all standard keyboard input. The only exception is the grab-drag interaction.
