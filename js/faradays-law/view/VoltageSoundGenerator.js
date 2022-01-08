f4d83f856c8da123927cc841e59b116f4d2b8350
phet-dev <phetadmin@gmail.com>
2022-01-08 03:18:41 -0700

    update copyright dates from daily grunt work

--- js/faradays-law/view/VoltageSoundGenerator.js
+++ js/faradays-law/view/VoltageSoundGenerator.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * VoltageSoundGenerator is a sound generator that produces sounds based on the voltage level.
