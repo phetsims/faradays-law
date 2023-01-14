aecfb09fc984104a179f9729350b8f9c9abdb7d6
phet-dev <phetadmin@gmail.com>
2023-01-14 04:22:20 -0700

    update copyright dates from daily grunt work

--- js/faradays-law/view/ControlPanelNode.js
+++ js/faradays-law/view/ControlPanelNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Strip of controls at the bottom of the screen, which are not shown in a visible panel.  It contains controls
