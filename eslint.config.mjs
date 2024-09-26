1fdd5ee4f7ea7e31534872f0404fa4fb37818114
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:16 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for faradays-law.
+ * ESLint configuration for faradays-law.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
